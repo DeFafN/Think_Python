34aa4a05eec7f600801c0b768cc6be7e0ea16776
mak <fafnir_master@mail.ru>
2025-10-13 00:24:20 +0300

    решены упражнения третьей главы

---
+++ Chapters/Chapter_Three/exercises_chapter3.py
@@ -0,0 +1,86 @@
+#Exercise3.1 Chapter 3
+
+string = input('Введите строку: ')
+def right_justify(string):
+        print(' ' * (70 - len(string)) + string)
+right_justify(string)
+
+#Exercise3.2 Chapter 3
+
+def do_twice (f):
+    f()
+    f()
+
+def print_spam():
+    print('спам')
+do_twice(print_spam)
+
+def do_twice_modify(func, value):
+    print_spam()
+    print_spam()
+
+do_twice_modify(print_spam, "spam")
+
+def print_twice(text):
+    print(text)
+    print(text)
+
+def do_twice_modify2(func, value):
+    print(value)
+    print(value)
+
+
+do_twice_modify2(print_twice, 'spam')
+
+def do_four(func, value):
+    do_twice_modify2(func, value)
+    do_twice_modify2(func, value)
+
+do_four(do_twice_modify2, 'Спам')
+
+#Exercise3.3 Chapter 3
+def print_raw():
+    print('+ ', '- '*4, '+', end='')
+    print(' ', '- '*4, '+')
+def print_colum():
+    print('|', ' '*9, '|', ' '*9, '|')
+
+print_raw()
+print_colum()
+print_colum()
+print_colum()
+print_raw()
+print_colum()
+print_colum()
+print_colum()
+
+#grid 4x4
+raw = '+ ' + 4 * '- '
+colum = '|' + 9 * ' '
+
+def print_plus_minus(raw):
+    print(3 * raw + "+", sep='',)
+
+def print_line(colum):
+    print(3* colum + '|', sep='')
+
+def do_twice(func, value):
+    func(value)
+    func(value)
+
+def print_spam(text):
+    print(text)
+
+def do_four(func, value):
+    do_twice(func, value)
+    do_twice(func, value)
+
+print_plus_minus(raw)
+do_four(print_line, colum)
+print_plus_minus(raw)
+do_four(print_line, colum)
+print_plus_minus(raw)
+do_four(print_line, colum)
+print_plus_minus(raw)
+do_four(print_line, colum)
+print_plus_minus(raw)
