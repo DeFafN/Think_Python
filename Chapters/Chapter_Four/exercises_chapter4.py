33a7fd85cffbc8156a351ce75d25b4aac6ae6dc1
mak <fafnir_master@mail.ru>
2025-10-14 21:32:21 +0300

    Сделал квадрат

---
+++ Chapters/Chapter_Four/exercises_chapter4.py
@@ -0,0 +1,16 @@
+import turtle
+bob = turtle.Turtle()
+
+# Exercise 1 Chapter 4
+
+def square(t):
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+square(bob)
+
+
+
+
+
+turtle.mainloop()
