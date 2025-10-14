02e4a58f60252a6b807522de01bd09f9305cbb26
mak <fafnir_master@mail.ru>
2025-10-14 21:35:03 +0300

    улучшил квадрат

--- Chapters/Chapter_Four/exercises_chapter4.py
+++ Chapters/Chapter_Four/exercises_chapter4.py
@@ -3,11 +3,11 @@ bob = turtle.Turtle()
 
 # Exercise 1 Chapter 4
 
-def square(t):
+def square(t, length):
     for i in range(4):
-        t.fd(100)
+        t.fd(length)
         t.lt(90)
-square(bob)
+square(bob, 100)
 
 
 
