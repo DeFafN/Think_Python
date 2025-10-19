9f38d422cdf780afacfff971e3613778d86d1410
mak <fafnir_master@mail.ru>
2025-10-19 18:14:50 +0300

    почему он начинает рисовать цветы?

--- Chapters/Chapter_Four/exercises_chapter4_pies.py
+++ Chapters/Chapter_Four/exercises_chapter4_pies.py
@@ -1,6 +1,5 @@
 import turtle
 import math
-import exercises_chapter4_flowers
 
 charle = turtle.Turtle()
 charle.hideturtle()
@@ -22,15 +21,20 @@ def polypie(charle, length, number_of_segments):
         triangle(charle, length, angle / 2)
         charle.left(angle)
 
+def move(charle, length):
+    charle.penup()
+    charle.fd(length)
+    charle.pendown()
+
 charle.fillcolor("red")
 charle.begin_fill()
 polypie(charle, 50, 7)
 charle.end_fill()
-exercises_chapter4_flowers.move(charle, 100)
+move(charle, 100)
 charle.fillcolor("green")
 charle.begin_fill()
 polypie(charle, 50, 6)
-exercises_chapter4_flowers.move(charle, 100)
+move(charle, 100)
 charle.end_fill()
 charle.fillcolor("blue")
 charle.begin_fill()
