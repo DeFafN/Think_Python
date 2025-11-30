ba11eaea7912350a6905745a0ff6255d8781a18e
Artem aka Karl <fafnir_master@mail.ru>
2025-11-30 13:18:17 +0300

    Ферма с input

---
+++ Chapters/Chapter5/chapter5_exersise2.py
@@ -0,0 +1,17 @@
+def check_fermat(a,b,c,n):
+    if n > 2:
+        if a**n + b**n == c**n:
+            print("святые воробьи. Ферма ошибся")
+        else:
+            print("Не, не работает")
+
+def get_input():
+    a = int(input("Введите целое число a: "))
+    b = int(input("Введите целое число b: "))
+    c = int(input("Введите целое число c: "))
+    n = int(input("Введите степень n (должна быть больше 2): "))
+    if n <= 2:
+        print("Степень должна быть больше 2")
+    check_fermat(a,b,c,n)
+
+get_input()
