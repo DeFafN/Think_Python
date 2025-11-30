b8637bf726e21b52433bbda0d13304157faf5321
Artem aka Karl <fafnir_master@mail.ru>
2025-11-30 13:50:58 +0300

    Стороны треугольника, вместе сила

---
+++ Chapters/Chapter5/chapter5_exersise4.py
@@ -0,0 +1,9 @@
+def recurse(n, s):
+    ''' n - количество рекурсий, s - сумма всех n, когда n == 0, выводится s '''
+    if n == 0:
+        print(s)
+    else:
+        recurse(n - 1, n + s)
+
+recurse(3, 0)
+
