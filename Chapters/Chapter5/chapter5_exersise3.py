0f458758bb009ed5d4077aefd9129b158e791673
Artem aka Karl <fafnir_master@mail.ru>
2025-11-30 13:40:48 +0300

    Стороны треугольника, вместе сила

---
+++ Chapters/Chapter5/chapter5_exersise3.py
@@ -0,0 +1,17 @@
+def is_triangle(a, b, c):
+    possible = ('Да')
+    impossible = ('Нет')
+    if a > b + c or b > a + c or c > a + b:
+        return impossible
+    else:
+        return possible
+
+
+def user_input():
+    a = int(input('Введите длину первого отрезка: '))
+    b = int(input('Введите длину второго отрезка: '))
+    c = int(input('Введите длину третьего отрезка: '))
+    result = is_triangle(a, b, c)
+    print(result)
+
+user_input()
