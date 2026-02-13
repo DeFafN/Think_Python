76ca4933bd711330e09df2af5f53b9dbe527f71a
Artem aka Karl <fafnir_master@mail.ru>
2026-02-14 00:20:18 +0300

    второй вариант функции arc

--- Chapters/Chapter4/ex4.1.py
+++ Chapters/Chapter4/ex4.1.py
@@ -1,6 +1,8 @@
 def arc(t: turtle.Turtle, radius: float, angle: float):
-    arc_length = 2 * math.pi * radius * angle / 360
-    n = int(arc_length / 3) + 1
+    arc_length = 2 * math.pi * radius * abs(angle) / 360
+    n = int(arc_length / 4) + 3
     step_length = arc_length / n
     step_angle = angle / n
+    t.left(step_angle / 2)
     polyline(t, n, step_length, step_angle)
+    t.right(step_angle / 2)
