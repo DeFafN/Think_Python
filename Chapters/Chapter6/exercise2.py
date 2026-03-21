2c12512ea9463ded6d9560e375de8c3ff8543588
Artem aka Karl <fafnir_master@mail.ru>
2026-03-21 17:11:35 +0300

    сделал 6.3

--- Chapters/Chapter6/exercise2.py
+++ Chapters/Chapter6/exercise2.py
@@ -1,4 +1,5 @@
-def ack(m, n):
+def ack(m: int, n: int):
+    '''Вычисляет функцию Аккермана'''
     if m == 0:
         return n + 1
     elif m > 0 and n == 0:
@@ -6,4 +7,4 @@ def ack(m, n):
     elif m > 0 and n > 0:
         return ack(m - 1, ack(m, n - 1))
 
-print(ack(5, 6))
+print(ack(3, 6)) # при m = 4, n = 7 достигается максимальная глубина рекурсии
