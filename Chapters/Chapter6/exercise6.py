6c1702de913614587bb1cfecf8158894f75f812b
Artem aka Karl <fafnir_master@mail.ru>
2026-03-21 17:42:44 +0300

    правильно назвал функцию в 6.6

--- Chapters/Chapter6/exercise6.py
+++ Chapters/Chapter6/exercise6.py
@@ -1,4 +1,4 @@
-def gratest_common_divisor(a: int, b: int) -> int:
+def gcd(a: int, b: int) -> int:
     """
     Возвращает наибольший общий делитель (НОД) двух чисел по алгоритму Евклида.
 
@@ -10,13 +10,13 @@ def gratest_common_divisor(a: int, b: int) -> int:
         int: НОД(a, b)
 
     Примеры:
-        gratest_common_divisor(48, 18)
+        gcd(48, 18)
         6
-        gratest_common_divisor(17, 13)
+        gcd(17, 13)
         1
     """
     if b == 0:
         return a
-    return gratest_common_divisor(b, a % b)
+    return gcd(b, a % b)
 
-print(gratest_common_divisor(48, 18))
+print(gcd(48, 18))
