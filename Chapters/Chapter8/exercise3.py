5f8964485f59fa90f24b8be375e3afe2767e561c
Artem aka Karl <fafnir_master@mail.ru>
2026-04-05 12:29:44 +0300

    8.3

--- Chapters/Chapter8/exercise3.py
+++ Chapters/Chapter8/exercise3.py
@@ -1,6 +1,23 @@
 def letter_counter(string, desired_letter):
+    """
+    Подсчитывает количество вхождений указанной буквы в строке.
+
+    Возвращает:
+        int: количество раз, сколько `desired_letter` встречается в `string`
+
+    Примеры:
+        >>> letter_counter('банан', 'а')
+        2
+        >>> letter_counter('Привет, Мир!', 'и')
+        2
+        >>> letter_counter('hello', 'l')
+        2
+        >>> letter_counter('hello', 'x')
+        0
+
+    """
     count = 0
     for letter in string:
-        if letter == desired_letter:
+        if letter.lower() == desired_letter.lower():
             count = count + 1
     return count
