0ab1c6e92b3888b71d22a7157879e21a8d37bca8
Artem aka Karl <fafnir_master@mail.ru>
2026-04-05 12:03:27 +0300

    8.2

--- Chapters/Chapter8/exercise2.py
+++ Chapters/Chapter8/exercise2.py
@@ -1,2 +1,22 @@
-my_string = "Банан"
-print(my_string.count("а"))
+def find(string: str, substring: str) -> int:
+    """Возвращает индекс вхождения символа `letter` в строку `string`.
+
+
+    Возвращает -1, если такого вхождения нет.
+
+
+    >>> find('abc', 'b')
+    2
+    >>> find('abc', 'c')
+    3
+    >>> find('abc', 'd')
+    -1
+    >>> find('hello', 'll')
+    3
+    """
+    index = 0
+    while index < len(string):
+        if string[index] == substring[0]:
+            return index + 1
+        index = index + 1
+    return -1
