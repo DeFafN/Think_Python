3636e39f3c6ca4b8370880c34b806ac189825dd0
Artem aka Karl <fafnir_master@mail.ru>
2026-04-05 13:52:31 +0300

    8.4

--- Chapters/Chapter8/exercise4.py
+++ Chapters/Chapter8/exercise4.py
@@ -2,6 +2,12 @@ def is_reverse(word1: str, word2: str) -> bool:
     """Проверить, являются ли word1 и word2 обращением друг друга
     >>> is_reverse('порт', 'троп')
     True
+    >>> is_reverse('кукушка', 'акшукук')
+    True
+    >>> is_reverse('папа', 'мама')
+    False
+    >>> is_reverse('ясень', 'тополь')
+    False
     """
     if len(word1) != len(word2):
         return False
